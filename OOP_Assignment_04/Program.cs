476dbcec932043565109f8a93e1d476169ccae41
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-16 23:17:24 +0300

    Solve Part(1)

--- OOP_Assignment_04/Program.cs
+++ OOP_Assignment_04/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
+            #region Part(1)
+            /*
+             
+             Q1* (b) To define a blueprint for a class
             
+             Q2* (a) private
+
+             Q3* (c) Only if they are static
+             
+             Q4* (b) Yes, interfaces can inherit from multiple interfaces
+             
+             Q5* (d) implements
+             
+             Q6* (a) Yes
+             
+             Q7* (b) No, all members are implicitly public
+             
+             Q8* (a) To hide the interface members from outside access
+             
+             Q9* (b) No, interfaces cannot have constructors
+             
+             Q10*(c) By separating interface names with commas
+
+            */
+
+            #endregion
             Console.ReadKey();
         }
     }
